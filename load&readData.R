d54e8b0fb0c6dbd2d5dd3aa80c6a6a7541ade5e7
CharlesPoulain <charlespoulaindo@yahoo.fr>
2016-12-26 02:19:49 +0100

    change in the load&readData.R file for next plots

--- load&readData.R
+++ load&readData.R
@@ -7,4 +7,10 @@ unlink(temp)
 ##here we have data,data frame containing all the data
 data_s<-data[data$Date=="1/2/2007" | data$Date=="2/2/2007",]
 ##we built a subset of data, data_s, that contain row with both asked dates
+data_s$DateTime<-paste(data_s$Date,data_s$Time)
+##data_s as it was in the previous step was enough for plot1.
+##but for the next plot I need to have a DateTime variable
+data_s$DateTime<-paste(data_s$Date,data_s$Time)
+data_s$DateTime<- strptime(data$Date&Time,"%d/%m/%Y %H:%M:%S")
+##we added a column DateTime combining Date and Time that R can understand
 
